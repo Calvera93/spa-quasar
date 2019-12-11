a39d13cba4d605a88fc4d3590bf9276876322f65
Calvera93 <34856473+Calvera93@users.noreply.github.com>
2019-12-11 18:46:07 -0500

    Add files via upload

---
+++ precache-manifest.6b4e8d78c33d8a468da9c527a4acf18a.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f7fa9e16b375d218b64",
+    "url": "css/9640cef0.83f60c6e.css"
+  },
+  {
+    "revision": "8dbadd4e0e8f8044d12a",
+    "url": "css/a39d306e.7e595b2c.css"
+  },
+  {
+    "revision": "724639f6f6ee6cf86dd7",
+    "url": "css/app.8c324220.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "c2b7ba9f705501f89f977951d8f8a78d",
+    "url": "index.html"
+  },
+  {
+    "revision": "e0a926103ea90562533a",
+    "url": "js/2d0f1530.95bff7e8.js"
+  },
+  {
+    "revision": "ce7a68105a6b9ae1e40e",
+    "url": "js/4b47640d.eed26305.js"
+  },
+  {
+    "revision": "2f7fa9e16b375d218b64",
+    "url": "js/9640cef0.989658eb.js"
+  },
+  {
+    "revision": "8dbadd4e0e8f8044d12a",
+    "url": "js/a39d306e.dc24a3ab.js"
+  },
+  {
+    "revision": "724639f6f6ee6cf86dd7",
+    "url": "js/app.93cd7753.js"
+  },
+  {
+    "revision": "35fe8c4a1870279a9b60",
+    "url": "js/vendor.aa4b0190.js"
+  },
+  {
+    "revision": "13b52461692e5d54e070614803d415d6",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "bde75c95c26cbe407d8c3a51149c9047",
+    "url": "statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1e6959a9778d40656217fe5eb7e0b7aa",
+    "url": "statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e776afac4479ee2ad8d6a0397661bf09",
+    "url": "statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "37ab9045dbdc0edf12ea59b3772e19f9",
+    "url": "statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "65d8571ec508d854ae4132e6540c476a",
+    "url": "statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "7233d85b329c161211830b1b30fdf0ec",
+    "url": "statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "c7428f8eba0499407710120fea46ead7",
+    "url": "statics/icons/favicon-96x96.png"
+  },
+  {
+    "revision": "04671eaea0c2fc94e31c5ab76ec67d08",
+    "url": "statics/icons/favicon.ico"
+  },
+  {
+    "revision": "805b0a66d63db95a76ddda23e3a8c12f",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "3f5de7654b25e5f1640662111d28b490",
+    "url": "statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "15bc9eca3421a7d5a0599a5fb39fe7a8",
+    "url": "statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "f309ab32f7ba3a2d3f50d05ba3d2aaa2",
+    "url": "statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "c8fdfe20437ccd115ee07cb684685ded",
+    "url": "statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "6624870bb64b1a562e8438e57cdc636a",
+    "url": "statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "ba58d8b825312a31114fb91484dadbeb",
+    "url": "statics/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "0931c3b2081ba4341956942f0d111c06",
+    "url": "statics/nieve.jpg"
+  },
+  {
+    "revision": "381f26aa56dc1925414a3fcf9e5b5e85",
+    "url": "statics/paisaje.jpg"
+  },
+  {
+    "revision": "549e5a2e83cb3a994a733ac3beee82a2",
+    "url": "statics/user.jpg"
+  }
+]);
